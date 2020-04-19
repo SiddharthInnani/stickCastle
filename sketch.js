3bc27ebcdac35647f1ed2e5d149087288c4d83a0
SiddharthInnani <62595976+SiddharthInnani@users.noreply.github.com>
2020-04-19 19:53:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var BillGates;
+
+
+function setup() {
+  createCanvas(800,400);
+  var b = createSprite(100,350,100,100);
+  var m = createSprite(201,350,100,100);
+  var a = createSprite(302,350,100,100);
+  var t = createSprite(403,350,100,100);
+  var e = createSprite(404,350,100,100);
+  var r = createSprite(505,350,100,100);
+  var q = createSprite(606,350,100,100);
+  var p = createSprite(707,350,100,100);
+  var f = createSprite(150,249,100,100);
+  var g = createSprite(251,249,100,100);
+  var l = createSprite(352,249,100,100);
+  var c = createSprite(453,249,100,100);
+  var z = createSprite(554,249,100,100);
+  var x = createSprite(655,249,100,100);
+  var as = createSprite(200,148,100,100);
+  var te = createSprite(301,148,100,100);
+  var cq = createSprite(402,148,100,100);
+  var egh = createSprite(503,148,100,100);
+  var yf = createSprite(604,148,100,100);
+
+
+}
+
+function draw() {
+  background(0);
+
+
+
+
+  drawSprites();
+}
